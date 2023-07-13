33632f7278c3c0b8cb0d435813d1d05d8081f0c1
benstanfish <benstanfish@hotmail.com>
2023-07-13 10:24:13 -0700

    .

--- utilities.py
+++ utilities.py
@@ -1,7 +1,5 @@
-import os, re
+import os
 from datetime import datetime
-
-
 
 displays = {
             'VGA': (640,480),
@@ -15,17 +13,32 @@ displays = {
             '4K':(3840,2160)
             }
 
-
-def getTimeStamp(showMS: bool = False):
-    if showMS == True:
-        return datetime.now().strftime('%Y-%m-%d--%H%M%S.%f')
-    else:
-        return datetime.now().strftime('%Y-%m-%d--%H%M%S')
-
+def getTimeStamp():
+    return datetime.now().strftime('%Y-%m-%d--%H%M%S')
 
 def buildPath(path):
     if os.path.exists(path) == False:
         os.mkdir(path)
     else:
-        new_path = path + " " + getTimeStamp()
-        os.mkdir(new_path)
+        path = nextPath(path)
+        os.mkdir(path)
+
+def nextPath(path):
+    """Creates a candidate path name that will not conflict with indexed predecessors.
+    Uses log(n) time algorithm to check existence of predecessor names (rather than
+    using sequential iteration)
+
+    Args:
+        path (string): path
+
+    Returns:
+        string: next path
+    """
+    i = 1
+    while os.path.exists(path + " " + str(i)):
+        i = i * 2
+    a, b = (i // 2, i)
+    while a + 1 < b:
+        c = (a + b ) // 2
+        a, b = (c, b) if os.path.exists(path + " " + str(c)) else (a, c)
+    return path + " " + str(b)
