744b138b910b63cb4b7ed8c56ce87b2ca68b1fe5
benstanfish <benstanfish@hotmail.com>
2023-07-26 13:21:14 -0700

    .

--- ttime.py
+++ ttime.py
@@ -1,9 +1,8 @@
 """Module for testing time functions"""
 __version__ = "0.0.1"
 
-import time
-import datetime
 import tlogger as tl
+import tutilities as tu
 
 tl.logger.info("Test Initialized.")
 
